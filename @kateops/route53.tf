875ae4944cc2ed40a3c1a9e0923d2972e2973932
Netzwolke <dev@netzwolke.com>
2023-05-02 07:54:22 +0200

    try new pipeline

--- @kateops/route53.tf
+++ @kateops/route53.tf
@@ -10,8 +10,16 @@ resource "aws_ses_domain_identity" "kateops" {
 }
 
 
-resource "aws_ses_domain_identity" "netzwolke" {
-  domain = aws_route53_zone.netzwolke.name
+#resource "aws_ses_domain_identity" "netzwolke" {
+#  domain = aws_route53_zone.netzwolke.name
+#}
+
+resource "aws_route53_record" "kateops_noip" {
+  name    = aws_route53_zone.kateops.name
+  type    = "MX"
+  zone_id = aws_route53_zone.kateops.zone_id
+  ttl     = "600"
+  records = ["5 mx.noip.com"]
 }
 
 resource "aws_route53_record" "kateops_ses" {
@@ -22,23 +30,23 @@ resource "aws_route53_record" "kateops_ses" {
   records = [aws_ses_domain_identity.kateops.verification_token]
 }
 
-resource "aws_route53_record" "netzwolke_ses" {
-  name    = "_amazonses.${aws_route53_zone.netzwolke.name}"
-  type    = "TXT"
-  zone_id = aws_route53_zone.netzwolke.zone_id
-  ttl     = "600"
-  records = [aws_ses_domain_identity.netzwolke.verification_token]
-}
+#resource "aws_route53_record" "netzwolke_ses" {
+#  name    = "_amazonses.${aws_route53_zone.netzwolke.name}"
+#  type    = "TXT"
+#  zone_id = aws_route53_zone.netzwolke.zone_id
+#  ttl     = "600"
+#  records = [aws_ses_domain_identity.netzwolke.verification_token]
+#}
 
 resource "aws_ses_domain_dkim" "kateops" {
   domain = aws_ses_domain_identity.kateops.domain
 }
 
-resource "aws_ses_domain_dkim" "netzwolke" {
-  domain = aws_ses_domain_identity.netzwolke.domain
-}
+#resource "aws_ses_domain_dkim" "netzwolke" {
+#  domain = aws_ses_domain_identity.netzwolke.domain
+#}
 
-resource "aws_route53_record" "kateops" {
+resource "aws_route53_record" "kateops_dkim" {
   count   = 3
   zone_id = aws_route53_zone.kateops.zone_id
   name    = "${aws_ses_domain_dkim.kateops.dkim_tokens[count.index]}._domainkey"
@@ -46,12 +54,12 @@ resource "aws_route53_record" "kateops" {
   ttl     = "600"
   records = ["${aws_ses_domain_dkim.kateops.dkim_tokens[count.index]}.dkim.amazonses.com"]
 }
-
-resource "aws_route53_record" "netzwolke" {
-  count   = 3
-  zone_id = aws_route53_zone.netzwolke.zone_id
-  name    = "${aws_ses_domain_dkim.netzwolke.dkim_tokens[count.index]}._domainkey"
-  type    = "CNAME"
-  ttl     = "600"
-  records = ["${aws_ses_domain_dkim.netzwolke.dkim_tokens[count.index]}.dkim.amazonses.com"]
-}
+#
+#resource "aws_route53_record" "netzwolke_dkim" {
+#  count   = 3
+#  zone_id = aws_route53_zone.netzwolke.zone_id
+#  name    = "${aws_ses_domain_dkim.netzwolke.dkim_tokens[count.index]}._domainkey"
+#  type    = "CNAME"
+#  ttl     = "600"
+#  records = ["${aws_ses_domain_dkim.netzwolke.dkim_tokens[count.index]}.dkim.amazonses.com"]
+#}
