8baf347d99782caf818501726711541c4b7168b4
d.hauck <d.hauck@netzwolke.com>
2023-06-18 01:08:02 +0200

    add vault injector

--- @kateops/@cluster-setup/vault-setup/resource.tf
+++ @kateops/@cluster-setup/vault-setup/resource.tf
@@ -5,7 +5,43 @@ resource "vault_auth_backend" "kubernetes" {
 resource "vault_kubernetes_auth_backend_config" "kubernetes" {
   backend = vault_auth_backend.kubernetes.path
   kubernetes_host = var.kubernetes_host
-  kubernetes_ca_cert = var.kubernetes_ca_cert
+  kubernetes_ca_cert = kubernetes_secret.kubernetes_auth.data["ca.crt"]
+  token_reviewer_jwt = kubernetes_secret.kubernetes_auth.data["token"]
+  issuer = "https://kubernetes.default.svc"
+}
+
+resource "kubernetes_service_account" "kubernetes_auth" {
+  metadata {
+    name = "vault-auth-kubernetes-backend"
+    namespace = "security"
+  }
+}
+
+resource "kubernetes_secret" "kubernetes_auth" {
+  metadata {
+    name = "vault-auth-kubernetes-backend"
+    namespace = kubernetes_service_account.kubernetes_auth.metadata[0].namespace
+    annotations = {
+      "kubernetes.io/service-account.name": "vault-irsa"//kubernetes_service_account.kubernetes_auth.metadata[0].name
+    }
+  }
+  type = "kubernetes.io/service-account-token"
+}
+
+resource "kubernetes_cluster_role_binding" "kubernetes_auth" {
+  metadata {
+    name = "vault-auth-kubernetes-backend-binding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "system:auth-delegator"
+  }
+  subject {
+    kind = "ServiceAccount"
+    name = "vault-irsa"
+    namespace = kubernetes_service_account.kubernetes_auth.metadata[0].namespace
+  }
 }
 
 resource "vault_kubernetes_auth_backend_role" "kubernetes" {
