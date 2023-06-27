88cf56706fbc5e6482a99b9b41621759d0dace9d
Daniel Hauck <d.hauck@netzwolke.com>
2023-06-27 00:20:17 +0000

    Add IT-scharfenberger and feedback-api

---
+++ @kateops/@client/module-client-it-scharfenberger.tf
@@ -0,0 +1,12 @@
+module "it-scharfenberger" {
+  source = "../../module/cluster/client"
+  name   = "IT Scharfenberger"
+  cluster_certificate_authority_data = local.cluster_certificate_authority_data
+  cluster_endpoint = local.cluster_endpoint
+  cluster_name = local.cluster_name
+}
+
+module "it-scharfenberger-projects" {
+  source = "./module-it-scharfenberger-projects"
+  group_id = module.it-scharfenberger.group_id
+}
