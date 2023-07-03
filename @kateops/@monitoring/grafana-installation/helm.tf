468913408ed95690045911257410b3c9d938c2e3
d.hauck <d.hauck@netzwolke.com>
2023-07-03 08:12:07 +0200

    fix keda scaling and add pod anti affinity

--- @kateops/@monitoring/grafana-installation/helm.tf
+++ @kateops/@monitoring/grafana-installation/helm.tf
@@ -2,7 +2,7 @@ resource "helm_release" "grafana" {
   chart = "grafana"
   name  = local.name
   namespace = var.namespace
-
+  version = "6.52.9"
   repository = "https://grafana.github.io/helm-charts"
   values = concat(local.templates, local.secret_templates)
 }
