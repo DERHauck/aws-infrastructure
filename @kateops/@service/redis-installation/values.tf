4de5942d5146c9ab89a06e0fec4172b2c4f2ea65
d.hauck <d.hauck@netzwolke.com>
2023-10-21 16:23:17 +0200

    fix gitlab and remove old stuff

--- @kateops/@service/redis-installation/values.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  vars = {
-    redis_secret_name: kubernetes_secret.redis_secret.metadata[0].name
-    redis_secret_key: local.password_secret_key
-    monitoring_namespace: var.monitoring_namespace
-  }
-  templates = [ for v in fileset("${path.module}/values","*"): templatefile("${path.module}/values/${v}", local.vars) ]
-  secret_templates = [] # [ for v in fileset("${path.module}/values","*.yml"): templatefile("${path.module}/values/${v}", local.secrets) ]
-}
